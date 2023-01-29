a935517fea6ac6f2ad1fe2b715b9f159b240edd1
Hcx-2008 <1106735030@qq.com>
2023-01-30 00:42:14 +0800

    Demo添加

--- nacosconfig/src/main/java/com/example/nacosconfig/controller/ConfigClientController.java
+++ nacosconfig/src/main/java/com/example/nacosconfig/controller/ConfigClientController.java
@@ -1,8 +1,12 @@
 package com.example.nacosconfig.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.example.nacosconfig.handler.CustomerBlockHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -38,6 +42,38 @@ public class ConfigClientController {
             throw new RuntimeException("异常比例测试");
         }
         return "------------testD";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "handler_HotKey")
+    public String testHotKey(@RequestParam(value = "hot1",required = false) String hot1,
+                             @RequestParam(value = "hot2",required = false)String hot2,
+                             @RequestParam(value = "hot13",required = false) String hot3){
+        return "----testHotKey,热点规则测试";
+    }
+
+    //处理异常方法，方法签名要和对应的接口方法保持一致
+    public String handler_HotKey(String hot1, String hot2, String hot3, BlockException exception){
+        return "系统繁忙稍后重试。。";
+    }
+
+    @GetMapping("/byRest")
+    @SentinelResource(value = "byRest")
+    public String byRest(){
+        // 自定义限流测试
+        return "-----byRest";
+    }
+
+    /**
+     * 此方法用到了自定义限流处理类型CustomerBlockHandler
+     * 中的handlerException1方法来处理限流逻辑。
+     */
+    @GetMapping("/bycustomer")
+    @SentinelResource(value = "bycustomer",
+            blockHandlerClass = CustomerBlockHandler.class,
+            blockHandler = "handlerException1")
+    public String bycustomer(){
+        return "-----bycustomer";
     }
 
 }
---
+++ nacosconfig/src/main/java/com/example/nacosconfig/handler/CustomerBlockHandler.java
@@ -0,0 +1,12 @@
+package com.example.nacosconfig.handler;
+
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
+public class CustomerBlockHandler {
+    public static String handlerException1(BlockException exception){
+        return "handlerException1：系统异常，请稍后重试！";
+    }
+    public static String handlerException2(BlockException exception){
+        return "handlerException2：网络崩溃了，请稍后重试！";
+    }
+}
