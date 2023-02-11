b3525e6634aec924dda6fa3d9308981e1c80181d
Hcx-2008 <1106735030@qq.com>
2023-02-12 01:06:19 +0800

    Sentinel熔断服务部署

--- cloudalibaba-consumer8084/src/main/java/com/example/controller/DemoController.java
+++ cloudalibaba-consumer8084/src/main/java/com/example/controller/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.example.cloudalibabacommons.utils.JsonResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +22,28 @@ public class DemoController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/fallback/{id}")
+    //同时添加SentinelResource注解的fallback和blockHandler属性
+    @SentinelResource(value = "fallback", fallback = "fallbackHandler", blockHandler = "blockHandler")
     public JsonResult<String> fallback(@PathVariable Long id) {
-        //通过Ribbon发起远程访问，访问9003/9004
-        return restTemplate.getForObject(serviceUrl+"/info/"+id, JsonResult.class);
+        if (id <= 3) {
+            //通过Ribbon发起远程访问，访问9003/9004
+            JsonResult<String> result = restTemplate.getForObject(serviceUrl + "/info/" + id, JsonResult.class);
+            System.err.println(result.getData());
+            return result;
+        } else {
+            throw new NullPointerException("没有对应的数据记录");
+        }
+    }
+
+    //处理Java异常
+    public JsonResult<String> fallbackHandler(Long id, Throwable e) {
+        JsonResult<String> result = new JsonResult<>(444, "NullPointerException异常", e.getMessage());
+        return result;
+    }
+
+    //处理Sentinel限流
+    public JsonResult<String> blockHandler(Long id, BlockException e) {
+        JsonResult<String> result = new JsonResult<>(445, "BlockException", e.getMessage());
+        return result;
     }
 }
